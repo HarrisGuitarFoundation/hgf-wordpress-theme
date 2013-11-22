055f7813eb4593f45cd8549a64f4d21e4e3264f2
Alex Harris <alexharris6@gmail.com>
2013-11-22 10:28:24 -0800

    initial commit for hgf theme

---
+++ page.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+	
+	<!-- section -->
+	<section role="main" class="main">
+	
+		<h1><?php the_title(); ?></h1>
+	
+	
+		<!-- article -->
+		<article>
+			
+			<h2> Some stuff</h2>
+			
+		</article>
+		<!-- /article -->
+	
+	
+	</section>
+	<!-- /section -->
+	
+<?php get_footer(); ?>
