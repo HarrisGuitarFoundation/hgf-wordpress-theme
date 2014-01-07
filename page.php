0db3190c3315f1d4ecb06310106cf9807deb5168
Alex Harris <alexharris6@gmail.com>
2014-01-07 12:21:46 -0800

    add row styles

--- page.php
+++ page.php
@@ -6,8 +6,21 @@
 		<?php while ( have_posts() ) : the_post(); ?> 
 		<!-- article -->
 		<article>
-
-			<?php the_content(); ?>
+			<div class="row white-background">
+				<?php the_content(); ?>
+			</div>
+			<?php while(has_sub_field("additional-content")): ?>
+				<?php if(get_row_layout() == "tan-background"): // layout: Content ?>
+	 				<div class="row tan-background">
+						<?php the_sub_field("content"); ?>
+					</div>
+	 			<?php endif; ?>
+	 			<?php if(get_row_layout() == "white-background"): // layout: Content ?>
+	 				<div class="row">
+						<?php the_sub_field("content"); ?>
+					</div>
+	 			<?php endif; ?>
+ 			<?php endwhile; ?>
 		</article>
 		<!-- /article -->
 	
