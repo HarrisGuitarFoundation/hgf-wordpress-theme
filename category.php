f854eee5dca6eff110b7546b22e7da0e597c9b2b
alexharris <alexharris6@gmail.com>
2014-02-21 17:56:11 -0800

    remove sidebar from event page, remove leadin p style from contact page

--- category.php
+++ category.php
@@ -1,13 +1,12 @@
 <?php get_header(); ?>
 	
 	<!-- section -->
-	<section role="main" class="main">
+	<section role="main" class="main no-sidebar">
 			<div class="row white-background">
 				<?php get_template_part('loop'); ?>
 				<?php get_template_part('pagination'); ?>
 			</div>
 	</section>
-	<?php get_sidebar(); ?>
 	<!-- /section -->
 	
 
