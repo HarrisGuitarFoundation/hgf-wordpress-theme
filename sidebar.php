58503465334752b7c5fcff3fb04e44d5518d9829
alexharris <alexharris6@gmail.com>
2014-04-05 16:58:40 -0700

    clean up sidebar menu styles

--- sidebar.php
+++ sidebar.php
@@ -1,8 +1,31 @@
 <!-- sidebar -->
 <aside class="sidebar" role="complementary">
 
-	<?php //get_template_part('searchform'); ?>
-    		
+	<?php 
+
+		$defaults = array(
+			'theme_location'  => 'sidebar-menu',
+			'menu'            => '',
+			'container'       => 'div',
+			'container_class' => '',
+			'container_id'    => '',
+			'menu_class'      => 'menu',
+			'menu_id'         => '',
+			'echo'            => true,
+			'fallback_cb'     => 'wp_page_menu',
+			'before'          => '',
+			'after'           => '',
+			'link_before'     => '',
+			'link_after'      => '',
+			'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+			'depth'           => 0,
+			'walker'          => ''
+		);
+
+		wp_nav_menu( $defaults );
+
+	?>
+    	
 	<div class="sidebar-widget">
 		<?php if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('widget-area-1')) ?>
 	</div>
