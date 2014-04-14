f8e7ea61511ed4cd61bbcc8b33a70178f0b17cb1
alexharris <alexharris6@gmail.com>
2014-04-14 14:49:07 -0700

    add events in homepage template

---
+++ events-homepage-template.php
@@ -0,0 +1,41 @@
+<?php get_header(); ?>
+	<!-- section -->
+	<section role="main" class="main">
+	
+	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+	
+		<!-- article -->
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		
+
+			
+			<!-- post title -->
+			<h1>
+				hello <?php the_title(); ?>
+			</h1>
+			<!-- /post title -->
+			
+			
+			<?php the_content(); // Dynamic Content ?>
+			
+		</article>
+		<!-- /article -->
+		
+	<?php endwhile; ?>
+	
+	<?php else: ?>
+	
+		<!-- article -->
+		<article>
+			
+			<h1><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h1>
+			
+		</article>
+		<!-- /article -->
+	
+	<?php endif; ?>
+	
+	</section>
+	<!-- /section -->
+
+<?php get_footer(); ?>
