d7595edb5cc32b4bd1ef2e9bf6ecbc5ab39dbe00
alexharris <alexharris6@gmail.com>
2014-04-16 16:12:22 -0700

    add additional content rows to default single view

--- single-default.php
+++ single-default.php
@@ -17,6 +17,19 @@
 			
 			
 			<?php the_content(); // Dynamic Content ?>
+
+			<?php while(has_sub_field("additional-content")): ?>
+			<?php if(get_row_layout() == "tan-background"): // layout: Content ?>
+					<div class="row tan-background">
+					<?php the_sub_field("content"); ?>
+				</div>
+				<?php endif; ?>
+				<?php if(get_row_layout() == "white-background"): // layout: Content ?>
+					<div class="row">
+					<?php the_sub_field("content"); ?>
+				</div>
+			<?php endif; ?>
+		<?php endwhile; ?>
 			
 		</article>
 		<!-- /article -->
